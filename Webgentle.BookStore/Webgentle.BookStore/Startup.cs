198d8f70d9a1d0eb36bcbc7d2444a9ea5179aa45
engr-asmaul <engr.asmaul.haque@gmail.com>
2020-05-22 17:05:02 +0600

    Class 20 is done

--- Webgentle.BookStore/Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Startup.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace Webgentle.BookStore
@@ -27,6 +30,15 @@ namespace Webgentle.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            
+            app.UseStaticFiles();
+
+          
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "MyStaticFiles")),
+                RequestPath = "/MyStaticFiles"
+            });
 
             app.UseRouting();
 
